83f0370e1a3b601b970b46bbfdba30b640782981
Tosin Omotayo <tosin.omotayo@fintraksoftware.com>
2020-06-17 12:00:24 +0100

    added new codes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,12 @@
 import { Component } from '@angular/core';
 
+interface IStudent {
+  firstname: string;
+  surname: string;
+  age: number;
+  id: number;
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +14,18 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'fintrakexam';
+
+  ngOninit() {
+
+  }
+
+  seedStudents () {
+    const student1: IStudent = '';
+    const student2: IStudent = '';
+    const student3: IStudent = '';
+    const student4: IStudent = '';
+    const student5: IStudent = '';
+
+    const allSeedStudents = [student1, student2, student3, student4, student5];
+  }
 }
